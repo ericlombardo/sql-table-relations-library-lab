4ff373bd35194c43cf70c549de83133341f3cf30
Eric <eric.m.lombardo@gmail.com>
2021-01-18 15:03:13 -0500

    Insert data into tables
    Inserted information in the series, sub-genres, authors, books,
    characters, and characters_books tables.

--- lib/insert.sql
+++ lib/insert.sql
@@ -14,8 +14,35 @@ INSERT INTO characters (name, motto, species, author_id) VALUES
 ("Harry Potter", "I'm not a Wizard?", "human", 1),
 ("Neville", "Have you seen my toad?", "human", 1),
 ("Hermione", "Honestly", "human", 1),
-("", "", "human", 1),
-("", "", "human", 1),
-("", "", "human", 1),
-("", "", "human", 1),
-("", "", "human", 1);
+("Ron", "Always with the tone of surprise", "human", 1),
+("Voldemort", "NOOOO!", "death", 1),
+("Legola", "They're taking the hobits to Isengard", "elf", 2),
+("Frodo", "Sam!", "hobit", 2),
+("Gimli", "Nobody tosses a dwarf!", "dwarf", 2);
+
+INSERT INTO subgenres (name) VALUES
+("fantasy"),
+("historical fantasy");
+
+INSERT INTO authors (name) VALUES
+("J.K Rowling"),
+("J.R.R. Tolken");
+
+INSERT INTO character_books (character_id, book_id) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(1, 2),
+(2, 2),
+(3, 2),
+(4, 2),
+(5, 2),
+(1, 3),
+(2, 3),
+(3, 3),
+(4, 3),
+(5, 3),
+(1, 4);
+
